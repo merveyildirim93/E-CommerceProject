9c666b55bebfd0bcdbdc3261448184220ad2919c
Merve Yıldırım <merveyildirimm93@gmail.com>
2021-12-22 02:44:23 +0300

    Add project files.

---
+++ E-Commerce.Data/Context/StoreContext.cs
@@ -0,0 +1,19 @@
+﻿using E_Commerce.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Data.Context
+{
+    public class StoreContext: DbContext
+    {
+        public StoreContext(DbContextOptions options):base(options)
+        {
+                
+        }
+        public DbSet<Product> Product { get; set; }
+    }
+}
---
+++ E-Commerce.Data/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Data.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string roductNames { get; set; }
+    }
+}
